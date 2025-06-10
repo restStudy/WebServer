c019e5839d9d405e07d7d9ed2ad0abd8b7a64708
restStudy <97348461@qq.com>
2025-06-11 04:00:39 +0800

    日常更新

--- Demo/DemoForm.Designer.cs
+++ Demo/DemoForm.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // DemoForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(959, 690);
+            Name = "DemoForm";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
